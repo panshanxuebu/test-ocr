cfb0e52ca599affb56d9ce29587221b78142e442
dongyuwei <newdongyuwei@gmail.com>
2021-03-02 13:26:17 +0800

    测试中文ocr文字识别

---
+++ README.md
@@ -0,0 +1,4 @@
+## 测试中文 ocr
+
+1. Download chi_sim.traineddata from https://github.com/tesseract-ocr/tessdata/blob/master/chi_sim.traineddata and put it in tessdata dir;
+2. cd tessdata then `gzip chi_sim.traineddata`
--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const Tesseract = require('tesseract.js');
 const fs = require('fs');
 
+// english
 // Tesseract.recognize(
 //   'https://tesseract.projectnaptha.com/img/eng_bw.png',
 //   'eng',
-//   { logger: m => console.log(m) }
+//   { logger: (m) => console.log(m) }
 // ).then(({ data: { text } }) => {
 //   console.log(text);
-// })
+// });
 
-Tesseract.recognize(
-  fs.readFileSync('/Users/yuwdong/Downloads/book.jpeg'),
-  'chi_sim',
-  {
-    logger: (m) => console.log(m),
-    langPath: '/Users/yuwdong/code/js/tessdata/',
-  }
-).then(({ data: { text } }) => {
-  console.log(text);
+Tesseract.recognize(fs.readFileSync('./test-momaterial/book.jpeg'), 'chi_sim', {
+  logger: (m) => console.log(m),
+  langPath: './tessdata/',
+}).then(({ data: { text } }) => {
+  console.log(text.replace(/(\n|\t|\s)/g, ''));
 });
